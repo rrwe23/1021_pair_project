77f8d611d3f030746b485b5b7c93803b84fccba9
rrwe23 <jhrrt0626@gmail.com>
2022-10-21 10:55:40 +0900

    accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
     return render(request, 'accounts/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+
+    else:
+        form = SignupForm()
+    context = {
+        'form' : form
+    }
+
+    return render(request,'accounts/signup.html',context)
+
